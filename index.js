9139cf74117b5ec2a2f122b26a00645cf046ed7e
Haico27 <haico.wensink@gmail.com>
2017-11-21 18:00:24 +0100

    function that counts number of alive neighbours added

--- index.js
+++ index.js
@@ -10,9 +10,14 @@ function createBoard(height, width) {
     for (var y = 1; y <= height; y++) {
       var cell = document.createElement("div");
       cell.className = "gridCell";
+      cell.id = "cell-" + x + "-" + y;
       cell.addEventListener("click", function(event) {
+        var x = event.target.id[5];
+        var y = event.target.id[7];
         event.target.classList.toggle('alive')
+
       })
+
       row.appendChild(cell);
     }
     board.appendChild(row)
@@ -20,3 +25,34 @@ function createBoard(height, width) {
 }
 
 createBoard(gridHeight, gridWidth)
+
+
+//function that returns the number of alive neighbours of a cell with coordinates x and y
+function numberOfAliveNeighbours(x,y) {
+  var aliveNeighbours = 0;
+
+  var self = document.getElementById('cell-' + x + '-' + y)
+
+  //don't count the cell itself, only its neighbours
+  if (self.classList.contains('alive')) {
+    aliveNeighbours--;
+  }
+
+
+  for (var i = x-1; i <= x+1; i++) {
+    for (var j = y-1; j <= y+1; j++) {
+
+      //the 10 stands for the gridHeight and gridWidth, needs to be changed when this function is called in runGame function
+      if (i>=0 && i<10 && j>=0 && j<10) {
+        var cellId = document.getElementById('cell-' + i + '-' + j)
+        console.log("cellId: ", cellId)
+        if(cellId.classList.contains('alive')) {
+          aliveNeighbours++;
+        }
+      }
+
+    }
+  }
+
+  return aliveNeighbours
+}
